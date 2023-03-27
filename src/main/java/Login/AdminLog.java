5d755347fd154f876175f9b7e5c812ebf8919418
Abu Sayed <abusayed102188@gmail.com>
2023-03-28 00:03:05 +0600

    new work

--- src/main/java/Login/AdminLog.java
+++ src/main/java/Login/AdminLog.java
@@ -21,13 +21,23 @@ public class AdminLog {
 
     @FXML
     void AdminDas(MouseEvent event) throws IOException {
-        FXMLLoader l11= new FXMLLoader(getClass().getResource("Admindash.fxml"));
-        Parent call=l11.load();
-        Admindash bc=l11.getController();
-        Scene sc1= new Scene(call);
-        Stage sc2=(Stage) adminlog.getScene().getWindow();
-        sc2.setScene(sc1);
-        sc2.show();
+        System.out.println("Want to in");
+
+        System.out.println("could not in");
+//        FXMLLoader l1= new FXMLLoader(getClass().getResource("Admindash.fxml"));
+//        Parent call=l1.load();
+//        Admindash bcc=l1.getController();
+//        Scene sc1= new Scene(call);
+//        Stage sc2=(Stage) adminlog.getScene().getWindow();
+//        sc2.setScene(sc1);
+//        sc2.show();
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("Admindash.fxml"));
+                Parent load= loader.load();
+        Admindash admin= loader.getController();
+        Scene scene= new Scene(load);
+        Stage scene2=(Stage) adminlog.getScene().getWindow();
+        scene2.setScene(scene);
+        scene2.show();
 
     }
     @FXML
--- src/main/java/Login/Admindash.java
+++ src/main/java/Login/Admindash.java
@@ -13,24 +13,23 @@ import java.io.IOException;
 
 
 public class Admindash {
-//    @FXML
-//    private Menu tofirst;
+
       @FXML
       private Button tofirst;
 
-    @FXML
-    void getoutt(ActionEvent event) throws IOException {
-
-    }
+    //@FXML
+//    void getoutt(ActionEvent event) throws IOException {
+//
+//    }
     @FXML
     void getoutt(MouseEvent event) throws IOException {
         FXMLLoader log= new FXMLLoader(getClass().getResource("Firstpage.fxml"));
         Parent admin=log.load();
         Firstpage b=log.getController();
-        Scene sc1= new Scene(admin);
-        Stage sc2=(Stage) tofirst.getScene().getWindow();
-        sc2.setScene(sc1);
-        sc2.show();
+        Scene sc11= new Scene(admin);
+        Stage sc22=(Stage) tofirst.getScene().getWindow();
+        sc22.setScene(sc11);
+        sc22.show();
 
     }
 
